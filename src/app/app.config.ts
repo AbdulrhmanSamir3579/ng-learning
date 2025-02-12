e5ff0ca8314c7d0346706d7a224923e0edde19cd
Abdulrahman <abdulrahman.aers@gmail.com>
2025-02-13 01:12:35 +0200

    install ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,19 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection, isDevMode} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {HttpClientModule} from '@angular/common/http';
 import {AppTranslateModule} from './shared/modules/app-translate.module';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({eventCoalescing: true}),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
-    importProvidersFrom(AppTranslateModule.forRoot())
-  ]
+    importProvidersFrom(AppTranslateModule.forRoot()),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
